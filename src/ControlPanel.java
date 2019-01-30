a3235805f3a107145a74eb152477f000ad1e1373
Maxym Brovarsky <asteixcube@gmail.com>
2019-01-30 12:36:38 +0200

    Update ControlPanel.java

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -1,11 +1,23 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class ControlPanel {
     private JButton deleteButton;
     private JButton addButton;
     private JButton deleteAllButton;
     private JButton AddAllButton;
+    private JTable menuTable;
+    private JTable productsTable;
     public ControlPanel() {
 
+    }
+    private void initButtons() {
+        deleteButton.addActionListener((ActionEvent e) -> {
+            int rowIndex = this.menuTable.getSelectedRow();
+            this.menuTable.remove(rowIndex);
+            //TODO: update data structeres
+
+        });
+        
     }
 }
